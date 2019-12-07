9a2d506cbb85dcd1daa2f92e1fe5d5a4f255d856
stratixx <stratixx@wp.pl>
2019-12-07 16:14:50 +0100

    bst remove wip

--- homework/1-bst/src/main.cpp
+++ homework/1-bst/src/main.cpp
@@ -101,7 +101,7 @@ public:
 
         while(curr!=nullptr)
         {
-            if(curr->key<newNode.key)
+            if(newNode.key<curr->key)
             {
                 if(curr->left!=nullptr)
                     curr = curr->left;
@@ -126,6 +126,141 @@ public:
 
     }
 
+    /*
+        Usuwa element o zgodnym kluczu
+    */
+    void remove( key_type key)
+    {
+        Node* parrent = nullptr;
+        Node* curr = root;
+
+        // szukanie elementu o zgodnym kluczu
+        while(curr!=nullptr)
+        {
+            if(curr->key==key) // klucz znaleziony
+                break;
+            if(key < curr->key) // klucz może być po lewej
+            {
+                if(curr->left!=nullptr) // krok w lewo
+                {
+                    parrent = curr;
+                    curr = curr->left;
+                }
+                else // nie znaleziono klucza
+                    curr = nullptr;
+            }
+            else // klucz moze być po prawej
+            {
+                if(curr->right!=nullptr) // krok w prawo
+                {
+                    parrent = curr;
+                    curr = curr->right;
+                }
+                else // nie znaleziono klucza
+                    curr = nullptr;        
+            }
+        }
+
+        if(curr==nullptr) // nie znaleziono klucza
+        {
+            return;
+        }
+
+        // w curr jest Node z pasującym kluczem
+        // w parrent jest rodzic curr
+
+        // jeśli usuwany node nie ma dzieci, skasuj
+        if(curr->left==nullptr && curr->right==nullptr)
+        {
+            if(parrent==nullptr) // usuwanie root
+            {
+                root = nullptr;
+            }
+            else
+            {
+                if(parrent->left==curr)
+                    parrent->left = nullptr;
+                else if( parrent->right==curr)
+                    parrent->right = nullptr;
+                else
+                    throw std::logic_error("Node has parrent, but parrent is not parrent of this child.");
+            }
+            delete curr;
+        }
+        else if(curr->left==nullptr) // node ma tylko prawe dziecko
+        {
+            if(parrent==nullptr) // usuwanie root
+            {
+                root = curr->right;
+            }
+            else
+            {    
+                if(parrent->left==curr)
+                    parrent->left = curr->right;
+                else if( parrent->right==curr)
+                    parrent->right = curr->right;
+                else
+                    throw std::logic_error("Node has parrent, but parrent is not parrent of this child.");
+            }
+            curr->right = nullptr;
+            delete curr;     
+        }
+        else if(curr->right==nullptr) // node ma tylko lewe dziecko
+        {
+            if(parrent==nullptr) // usuwanie root
+            {
+                root = curr->left;
+            }
+            else
+            {    
+                if(parrent->left==curr)
+                    parrent->left = curr->left;
+                else if( parrent->right==curr)
+                    parrent->right = curr->left;
+                else
+                    throw std::logic_error("Node has parrent, but parrent is not parrent of this child.");
+            }
+            curr->left = nullptr;
+            delete curr;            
+        }
+        else // node ma obydwoje dzieci
+        {
+            Node* next = curr->right; // next będzie następnikiem curr
+            Node* nextParrent = curr; // nextParrent będzie parrentem next
+
+            while(next->left!=nullptr) // szukanie następnika curr
+            {
+                nextParrent = next;
+                next = next->left;
+            }     
+            // //jeśli nastęþnik jest bezpośrednim dzieckiem usuwanego elementu
+            // if(curr->right==next)
+            // {
+                
+            // }       
+            // przepięcie prawego dziecka next do nextParrent
+            // if(nextParrent->left==next)
+            //     nextParrent->left = next->right;
+
+            // if(parrent==nullptr) // usuwanie root
+            // {
+            //     root = next;
+            //     root->left = curr->left;
+                
+            // }
+            // else
+            // {    
+            //     if(parrent->left==curr)
+            //         parrent->left = curr->left;
+            //     else if( parrent->right==curr)
+            //         parrent->right = curr->left;
+            //     else
+            //         throw std::logic_error("Node has parrent, but parrent is not parrent of this child.");
+            // }
+
+        }
+        
+    }
 
 	template<typename StreamType>
 	void print(StreamType& stream) const
